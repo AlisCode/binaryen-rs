86f332c270228eec39de4f70f3def906407043ed
Olivier Pinon <oliv.pinon@gmail.com>
2026-03-18 23:18:53 +0100

    Refactor type creation

--- binaryen/src/type_.rs
+++ binaryen/src/type_.rs
@@ -1,5 +1,7 @@
 pub use binaryen_sys::bindings::BinaryenType;
-use binaryen_sys::bindings::{BinaryenTypeCreate, BinaryenTypeInt32, BinaryenTypeNone};
+use binaryen_sys::bindings::{
+    BinaryenTypeArity, BinaryenTypeCreate, BinaryenTypeInt32, BinaryenTypeNone,
+};
 
 #[derive(Clone, Copy)]
 pub struct Type(BinaryenType);
@@ -13,21 +15,31 @@ impl Type {
         Type(unsafe { BinaryenTypeInt32() })
     }
 
+    pub fn create(types: Vec<Type>) -> Self {
+        let mut raw_value_types: Vec<BinaryenType> =
+            types.into_iter().map(Type::into_inner).collect();
+        let len = raw_value_types.len();
+
+        let ty = unsafe { BinaryenTypeCreate(raw_value_types.as_mut_ptr(), len as u32) };
+        Type(ty)
+    }
+
+    pub fn arity(&self) -> u32 {
+        unsafe { BinaryenTypeArity(self.0) }
+    }
+
     pub(crate) fn into_inner(self) -> BinaryenType {
         self.0
     }
 }
 
-#[derive(Default)]
-pub struct Types(Vec<Type>);
+#[cfg(test)]
+pub mod tests {
+    use super::*;
 
-impl Types {
-    pub fn create(&mut self, value_types: Vec<Type>) -> Type {
-        let idx = self.0.len();
-        let mut raw_value_types: Vec<BinaryenType> =
-            value_types.into_iter().map(Type::into_inner).collect();
-        let ty = unsafe { BinaryenTypeCreate(raw_value_types.as_mut_ptr(), idx as u32) };
-
-        Type(ty)
+    #[test]
+    fn type_arity() {
+        let ty = Type::create(vec![Type::i32(), Type::i32()]);
+        assert_eq!(ty.arity(), 2);
     }
 }
--- binaryen/tests/hello_world.rs
+++ binaryen/tests/hello_world.rs
@@ -1,20 +1,14 @@
 // A port of the c-api-hello-world example:
 // https://github.com/WebAssembly/binaryen/blob/899e44e2c3dd40795c843a91cfc326e11d64cab0/test/example/c-api-hello-world.c
 
-use binaryen::{
-    expression::operation::Operation,
-    module::Module,
-    type_::{Type, Types},
-};
+use binaryen::{expression::operation::Operation, module::Module, type_::Type};
 
 #[test]
 fn c_api_hello_world() {
     let module = Module::new();
 
     // Create a function type for  i32 (i32, i32)
-    let ii = vec![Type::i32(), Type::i32()];
-    let mut types = Types::default();
-    let params = types.create(ii);
+    let params = Type::create(vec![Type::i32(), Type::i32()]);
     let results = Type::i32();
 
     // Get the 0 and 1 arguments, and add them
