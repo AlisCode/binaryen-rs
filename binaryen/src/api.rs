84e2f410bc4d210c41dbd104254685afd117261b
Olivier Pinon <oliv.pinon@gmail.com>
2026-03-19 19:13:37 +0100

    Add basic debug-info accessor/setter for Binaryen api

--- binaryen/src/api.rs
+++ binaryen/src/api.rs
@@ -1,12 +1,31 @@
-use binaryen_sys::bindings::BinaryenSetColorsEnabled;
+use binaryen_sys::bindings::{
+    BinaryenGetDebugInfo, BinaryenSetColorsEnabled, BinaryenSetDebugInfo,
+};
 
 pub struct Binaryen;
 
 impl Binaryen {
+    // Enable or disable coloring for the Wasm printer
     pub fn set_colors_enabled(enabled: bool) {
         // SAFETY: No particular conditions to fulfill
         unsafe {
             BinaryenSetColorsEnabled(enabled);
+        }
+    }
+
+    // Gets whether generating debug information is currently enabled or not.
+    // Applies to all modules, globally.
+    pub fn get_debug_info() -> bool {
+        // SAFETY: No particular conditions to fulfill
+        unsafe { BinaryenGetDebugInfo() }
+    }
+
+    // Enables or disables debug information in emitted binaries.
+    // Applies to all modules, globally.
+    pub fn set_debug_info(enabled: bool) {
+        // SAFETY: No particular conditions to fulfill
+        unsafe {
+            BinaryenSetDebugInfo(enabled);
         }
     }
 }
